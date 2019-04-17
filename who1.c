ca77b77d990eb648a39159e81556632d688f16f9
eh9 <erich9331@163.com>
2019-04-17 22:47:10 +0800

    add show_info function

--- who1.c
+++ who1.c
@@ -6,6 +6,17 @@
 
 #define SHOWHOST 		//??
 
+void show_info(struct utmp* utp)
+{
+	printf("%-8.8s ", utp->ut_user);
+	printf("%-8.8s ", utp->ut_line);
+	printf("%-8.8s ", utp->ut_id);
+	printf("%10ld", utp->ut_tv.tv_sec);
+#ifdef SHOWHOST
+	printf("%-8.8s ", utp->ut_host);
+#endif
+	printf("\n");
+}
 
 int main(int argc, char** argv)
 {
@@ -20,13 +31,16 @@ int main(int argc, char** argv)
 	}
 	
 	while(reclen == read(utmpfd, &current_record, reclen))
-	{
+		show_info(&current_record);
+
+		/*	{
 		printf("%s", current_record.ut_line);
 		printf("\t%s", current_record.ut_id);
 		printf("\t%s", current_record.ut_user);
 		printf("\t%s", current_record.ut_host);
 		printf("\t%d\n", current_record.ut_pid);
-	}
+	}*/
+
 	close(utmpfd);
 	return 0;
 
