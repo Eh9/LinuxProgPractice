c00205290067de67063d9eb2914aac13903cc2d7
eh9 <erich9331@163.com>
2019-04-28 16:24:47 +0800

    a screen protection tools using curses.h

--- linuxOrder/scrpro.c
+++ linuxOrder/scrpro.c
@@ -1,25 +1,27 @@
 #include<curses.h>
 #include<stdio.h>
 #include<unistd.h>
+#include<string.h>
 
 int main(){
 	int 	a = 7;					//x step
 	int	b = 7;					//y step	
 	int	x = 0;					//x start
-	int	y = 0;					//y start
+	int	yleft = 0;					//y start
 	char 	strShow[] = "i am your father";		//show welcome
 	char 	blkShow[] = "                ";		//blank
+	int 	yright = yleft + strlen(strShow);
 
 
 	initscr();
 	clear();
-	for(; x >= 0 && x < LINES && y >= 0 && y < COLS; x += a, y += b){
-		if(x > 0 && x < LINES && y > 0 && y < COLS){
-			move(x - a, y - b);
+	for(; x >= 0 && x < LINES && yleft >= 0 && yright < COLS; x += a, yleft += b, yright += b){
+		if(x > 0 && x < LINES && yleft > 0 && yright < COLS){
+			move(x - a, yleft - b);
 			addstr(blkShow);
 			refresh();
 		}
-		move(x, y);
+		move(x, yleft);
 		if(x % 2 == 1)
 			standout();
 		addstr(strShow);
@@ -29,7 +31,7 @@ int main(){
 		sleep(1);
 		if(x + a >= LINES || x + a <= 0)
 			a = -a;
-		if(y + b >= COLS || y + b <= 0)
+		if(yright + b >= COLS || yleft + b <= 0)
 		       	b = -b;	
 	}
 	refresh();
